4e1445c1ea29d482736773fbfe06c1229e84c1ea
Ruben Mateus <ruben.mateus3@gmail.com>
2022-01-02 18:39:13 +0000

    feat: Add core functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import UserProvider from '../context/user'
+import Nav from "../components/nav";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Nav />
+      <Component {...pageProps} />
+    </UserProvider>
+  )
 }
 
 export default MyApp
